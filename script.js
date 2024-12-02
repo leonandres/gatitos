2b0fc267764df378eecd8fb566e453a8e089c6ff
Andrés León <andresleonb90@gmail.com>
2024-12-01 21:51:59 -0300

    Se agrega script.js con llamado a api de gatitos y se modifica index

---
+++ script.js
@@ -0,0 +1,24 @@
+function llamadoAApiDeFotosDeGatitoAleatoria(){
+    const container = document.getElementById("contenedor-foto");
+
+    const url = "https://api.thecatapi.com/v1/images/search?mime_types=jpg,png&size=med&limit=1";//Llamado a la api de gatitos: devuelve JPGs, tamanio med y solo 1 foto
+
+    fetch(url)
+        .then(response  => {
+            if(!response.ok){
+                console.log("Gatito no encontrado, vuelva a intentar.");
+            }
+            return response.json();
+            }
+        )
+        .then(data =>{
+            const img = document.createElement("img");
+            img.src = data[0].url;
+
+            container.appendChild(img);
+        
+        }
+            
+        )
+
+}
